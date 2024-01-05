1f11bd4492019ef4b4548d3e80c74a5a632455a1
akinayturan <akinayturan@gmail.com>
2024-01-05 14:37:51 +0300

    edt

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -44,7 +44,7 @@ export default function Footer() {
                 </div>
                 <div className="site-footer__bottom">
                     <div className="container">
-                        <p className="site-footer__copy">&copy; Copyright 2023 by <a target="_blank" href="https://sub.fyi/">Sub.fyi</a></p>
+                        <p className="site-footer__copy">&copy; Copyright {new Date().getFullYear()} by <a target="_blank" href="https://sub.fyi/">Sub.fyi</a></p>
                         <div className="site-footer__social">
                             <div onClick={scrollTop} className="scroll-to-target site-footer__scroll-top"><i
                                 className="fas fa-arrow-up"></i></div>
